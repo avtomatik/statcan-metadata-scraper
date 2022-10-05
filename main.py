06748a079395432243dc6fe6848270b4b7a1c885
Alexander Mikhailov <grey-machine@yandex.ru>
2022-10-05 18:05:01 +0300

    Made Use of bs4.element.Tag Class

--- main.py
+++ main.py
@@ -40,50 +40,30 @@ def main():
     -------
     None.
     '''
+    FILE_NAME = 'stat_can_all.xlsx'
     number_of_sources = get_number_of_sources()
-    _data = []
-    for _ in range(1+number_of_sources // 100):
+    data_list = []
+    for _ in range(1 + number_of_sources // 100):
         GENERIC_URL = 'https://www150.statcan.gc.ca/n1/en/type/data?count=100&p={}-All#all'
         page = requests.get(GENERIC_URL.format(_))
-        print(f'Parcing Page {1+_:3} Out of {1+number_of_sources // 100}')
+        print(f'Parsing Page {1+_:3} Out of {1+number_of_sources // 100}')
         soup = BeautifulSoup(page.text, 'lxml')
         details_soup = soup.find('details', id='all')
         items = details_soup.find_all('li', {'class': 'ndm-item'})
         for item in items:
-            former_id = item.find('div', class_='ndm-result-formerid')
-            geo = item.find('div', class_='ndm-result-geo')
-            frequency = item.find('div', class_='ndm-result-freq')
-            description = item.find('div', class_='ndm-result-description')
-            if former_id:
-                former_id = item.find(
-                    'div', class_='ndm-result-formerid'
-                ).get_text()
-            else:
-                former_id = None
-            if geo:
-                geo = item.find('div', class_='ndm-result-geo').get_text()
-            else:
-                geo = None
-            if frequency:
-                frequency = item.find(
-                    'div', class_='ndm-result-freq'
-                ).get_text()
-            else:
-                frequency = None
-            if description:
-                description = item.find(
-                    'div', class_='ndm-result-description'
-                ).get_text()
-            else:
-                description = None
-            _data.append(
+            tag_description = item.find('div', class_='ndm-result-description')
+            tag_former_id = item.find('div', class_='ndm-result-formerid')
+            tag_frequency = item.find('div', class_='ndm-result-freq')
+            tag_geo = item.find('div', class_='ndm-result-geo')
+
+            data_list.append(
                 {
                     'title': item.find('div', class_='ndm-result-title').get_text(),
                     'product_id': item.find('div', class_='ndm-result-productid').get_text(),
-                    'former_id': former_id,
-                    'geo': geo,
-                    'frequency': frequency,
-                    'description': description,
+                    'former_id': None if tag_former_id is None else tag_former_id.get_text(),
+                    'geo': None if tag_geo is None else tag_geo.get_text(),
+                    'frequency': None if tag_frequency is None else tag_frequency.get_text(),
+                    'description': None if tag_description is None else tag_description.get_text(),
                     'release_date': item.find('span', class_='ndm-result-date').get_text(),
                     'type': item.find(
                         'div',
@@ -93,15 +73,14 @@ def main():
                 }
             )
 
-    data = pd.DataFrame.from_dict(_data)
+    data = pd.DataFrame.from_dict(data_list)
     data[['id', 'title_only']] = data.iloc[:, 0].str.split(
         pat='. ',
         n=1,
         expand=True
     )
     data['id'] = pd.to_numeric(data['id'].str.replace(',', ''))
-    data.fillna('None', inplace=True)
-    data.to_excel('stat_can_all.xlsx', index=False)
+    data.fillna('None').to_excel(FILE_NAME, index=False)
 
 
 if __name__ == '__main__':
