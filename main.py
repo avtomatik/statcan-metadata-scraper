791f6dca806b56ab66341df30715e61999fa5f70
Alexander Mikhailov <grey-machine@yandex.ru>
2022-10-06 11:55:09 +0300

    Updated As Per scnerd's Feedback @ https://codereview.stackexchange.com/a/280230/263987

--- main.py
+++ main.py
@@ -15,38 +15,52 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_number_of_sources() -> int:
-    '''
+def get_number_of_sources(
+        url: str = 'https://www150.statcan.gc.ca/n1/en/type/data'
+) -> int:
+    """
     Retrieves Number of STATCAN Sources
+
+    Parameters
+    ----------
+    url : str, optional
+        Link. The default is 'https://www150.statcan.gc.ca/n1/en/type/data'.
 
     Returns
     -------
     int
         Number of STATCAN Sources.
 
-    '''
-    URL = 'https://www150.statcan.gc.ca/n1/en/type/data'
-    page = requests.get(URL)
+    """
+    page = requests.get(url)
     soup = BeautifulSoup(page.text, 'lxml')
     result = re.search(r'\((.*?)\)', soup.summary.get_text()).group(1)
     return int(result.replace(',', ''))
 
 
-def main():
-    '''
+def main(
+    url_generic: str = 'https://www150.statcan.gc.ca/n1/en/type/data?count=100&p={}-All#all',
+    file_name: str = 'stat_can_all.xlsx',
+) -> None:
+    """
     Builds Resulting DataFrame and Dumps It To Excel File
+
+    Parameters
+    ----------
+    url_generic : str, optional
+        DESCRIPTION. The default is 'https://www150.statcan.gc.ca/n1/en/type/data?count=100&p={}-All#all'.
+    file_name : str, optional
+        DESCRIPTION. The default is 'stat_can_all.xlsx'.
 
     Returns
     -------
-    None.
-    '''
-    FILE_NAME = 'stat_can_all.xlsx'
+    None
+    """
     number_of_sources = get_number_of_sources()
     data_list = []
     for _ in range(1 + number_of_sources // 100):
-        GENERIC_URL = 'https://www150.statcan.gc.ca/n1/en/type/data?count=100&p={}-All#all'
-        page = requests.get(GENERIC_URL.format(_))
-        print(f'Parsing Page {1+_:3} Out of {1+number_of_sources // 100}')
+        page = requests.get(url_generic.format(_))
+        print(f'Parsing Page {1 + _:3} Out of {1 + number_of_sources // 100}')
         soup = BeautifulSoup(page.text, 'lxml')
         details_soup = soup.find('details', id='all')
         items = details_soup.find_all('li', {'class': 'ndm-item'})
@@ -60,10 +74,10 @@ def main():
                 {
                     'title': item.find('div', class_='ndm-result-title').get_text(),
                     'product_id': item.find('div', class_='ndm-result-productid').get_text(),
-                    'former_id': None if tag_former_id is None else tag_former_id.get_text(),
-                    'geo': None if tag_geo is None else tag_geo.get_text(),
-                    'frequency': None if tag_frequency is None else tag_frequency.get_text(),
-                    'description': None if tag_description is None else tag_description.get_text(),
+                    'former_id': tag_former_id and tag_former_id.get_text(),
+                    'geo': tag_geo and tag_geo.get_text(),
+                    'frequency': tag_frequency and tag_frequency.get_text(),
+                    'description': tag_description and tag_description.get_text(),
                     'release_date': item.find('span', class_='ndm-result-date').get_text(),
                     'type': item.find(
                         'div',
@@ -80,7 +94,7 @@ def main():
         expand=True
     )
     data['id'] = pd.to_numeric(data['id'].str.replace(',', ''))
-    data.fillna('None').to_excel(FILE_NAME, index=False)
+    data.fillna('None').to_excel(file_name, index=False)
 
 
 if __name__ == '__main__':
